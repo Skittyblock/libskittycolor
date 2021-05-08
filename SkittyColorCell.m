9f78f0314995fd36ddc0c04c48a59971732734c2
Lavie <5081484+lgariv@users.noreply.github.com>
2021-05-08 21:58:17 +0300

    Update to use the native color picker when using iOS 14

--- SkittyColorCell.m
+++ SkittyColorCell.m
@@ -24,15 +24,45 @@
 - (void)openColorPicker {
 	// do it
 	UIViewController *viewController = [self _viewControllerForAncestor];
-	SkittyColorViewController *colorController = [[SkittyColorViewController alloc] initWithProperties:self.specifier.properties];
-	[viewController presentViewController:colorController animated:YES completion:nil];
+	if (@available(iOS 14, *)) {
+		UIColorPickerViewController *colorController = [[UIColorPickerViewController alloc] init];
+		NSMutableDictionary *settings = [[NSMutableDictionary alloc] initWithContentsOfFile:[NSString stringWithFormat:@"/var/mobile/Library/Preferences/%@.plist", self.specifier.properties[@"defaults"]]];
+		colorController.selectedColor = colorFromHexString([settings objectForKey:self.specifier.properties[@"key"]] ?: self.specifier.properties[@"default"]);
+		colorController.delegate = (id)self;
+		[viewController presentViewController:colorController animated:YES completion:nil];
+	} else {
+		SkittyColorViewController *colorController = [[SkittyColorViewController alloc] initWithProperties:self.specifier.properties];
+		[viewController presentViewController:colorController animated:YES completion:nil];
+	}
+}
+
+- (void)colorPickerViewControllerDidSelectColor:(UIViewController *)vc {
+	UIColorPickerViewController *viewController = (UIColorPickerViewController*)vc;
+	NSMutableDictionary *settings = [[NSMutableDictionary alloc] initWithContentsOfFile:[NSString stringWithFormat:@"/var/mobile/Library/Preferences/%@.plist", self.specifier.properties[@"defaults"]]];
+	[settings setObject:stringFromColor(viewController.selectedColor) forKey:self.specifier.properties[@"key"]];
+	[settings writeToURL:[NSURL URLWithString:[NSString stringWithFormat:@"file:///var/mobile/Library/Preferences/%@.plist", self.specifier.properties[@"defaults"]]] error:nil];
+
+	// UIView *colorPreview = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 26, 26)];
+	// colorPreview.layer.cornerRadius = colorPreview.frame.size.width / 2;
+	// colorPreview.layer.borderWidth = 1;
+	// colorPreview.layer.borderColor = [UIColor lightGrayColor].CGColor;
+	// colorPreview.backgroundColor = viewController.selectedColor;
+	// [self setAccessoryView:colorPreview];
+	[self updatePreview];
+
+	CFPreferencesSetAppValue((CFStringRef)self.specifier.properties[@"key"], (CFStringRef)stringFromColor(viewController.selectedColor), (CFStringRef)self.specifier.properties[@"defaults"]);
 }
 
 - (void)updatePreview {
-	UIView *colorPreview = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 29, 29)];
+	UIView *colorPreviewColoredBorder = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 32, 32)];
+	colorPreviewColoredBorder.layer.cornerRadius = colorPreviewColoredBorder.frame.size.width / 2;
+	colorPreviewColoredBorder.layer.borderWidth = 3;
+	UIView *colorPreview = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 20, 20)];
 	colorPreview.layer.cornerRadius = colorPreview.frame.size.width / 2;
-	colorPreview.layer.borderWidth = 1.5;
-	colorPreview.layer.borderColor = [UIColor lightGrayColor].CGColor;
+	// colorPreview.layer.borderWidth = 1;
+	// colorPreview.layer.borderColor = [UIColor labelColor].CGColor;
+	[colorPreviewColoredBorder addSubview:colorPreview];
+	colorPreview.center = colorPreviewColoredBorder.center;
 
 	NSMutableDictionary *settings = [[NSMutableDictionary alloc] initWithContentsOfFile:[NSString stringWithFormat:@"/var/mobile/Library/Preferences/%@.plist", self.specifier.properties[@"defaults"]]];
 	NSString *hex = [settings objectForKey:self.specifier.properties[@"key"]] ?: self.specifier.properties[@"default"];
@@ -42,9 +72,11 @@
 
 	UIColor *color = colorFromHexString(hex);
 
+	colorPreviewColoredBorder.backgroundColor = [UIColor clearColor];
+	colorPreviewColoredBorder.layer.borderColor = color.CGColor;
 	colorPreview.backgroundColor = color;
 
-	[self setAccessoryView:colorPreview];
+	[self setAccessoryView:colorPreviewColoredBorder];
 }
 
 - (void)didMoveToSuperview {
